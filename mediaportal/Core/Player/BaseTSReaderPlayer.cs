4eacc9de80d84174161e7366ac989bdd8b25bf73
sebastiii <sebastiii@team-mediaportal.com>
2016-09-01 20:42:48 +0200

    MP1-4781 : madVR V173 (Trying to fix regression with EVR 5:4 ratio issue)

--- mediaportal/Core/Player/BaseTSReaderPlayer.cs
+++ mediaportal/Core/Player/BaseTSReaderPlayer.cs
@@ -2080,7 +2080,7 @@ namespace MediaPortal.Player
             }
             DirectShowUtil.ReleaseComObject(pinFromOut); pinFromOut = null;
           }
-          else
+          else if (GUIGraphicsContext.VideoRenderer == GUIGraphicsContext.VideoRendererType.madVR)
           {
             if (selection == "Video")
             {
@@ -2127,6 +2127,12 @@ namespace MediaPortal.Player
               _graphBuilder.RemoveFilter(pInfo.filter);
               Log.Debug("TSReaderPlayer: UpdateFilters Remove filter - {0}", fInfo.achName);
             }
+          }
+          else
+          {
+            DirectShowUtil.DisconnectAllPins(_graphBuilder, pInfo.filter);
+            _graphBuilder.RemoveFilter(pInfo.filter);
+            Log.Debug("TSReaderPlayer: UpdateFilters Remove filter - {0}", fInfo.achName);
           }
           DsUtils.FreePinInfo(pInfo);
           DirectShowUtil.ReleaseComObject(fInfo.pGraph);
